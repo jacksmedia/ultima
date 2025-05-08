c1eb3c6600708b5adfa4cf1c9687ef870e372cd7
jacksmedia <alex@j4cks.com>
2025-05-08 12:44:50 -0700

    🔖 bootstrap added correctly

--- layout.tsx
+++ layout.tsx
@@ -29,18 +29,14 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
         <title>FF4 Ultima Patcher</title>
         <meta name="description" content="Get FF4 Ultima Plus" title="FF4 Ultima Patcher" />
         <link rel="icon" type="image/x-icon" href="/public/favicon.ico" />
-        {/* Bootstrap 5.3 for styling bc Tailwind is overrated */}
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" />
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" />
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"></script>
       </Head>
 
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-          <nav className="d-flex justify-content-left">
+          <nav className="nav d-flex justify-content-left ">
             {navLinks.map(({ href, label }) => (
               <Link key={href} href={href} passHref>
                 <span
-                  className={`  ${
+                  className={`nav-item  ${
                     router.pathname === href ? '' : ''
                   }`}
                 >
