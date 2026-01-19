0a6287408c766cefb0c630def06a029323f40cb8
jacksmedia <alex@j4cks.com>
2026-01-18 17:48:11 -0800

    ✨mobile layouts for 3 pages, add 1 missing guide

--- pages/discord.tsx
+++ pages/discord.tsx
@@ -8,14 +8,16 @@ const Discord: NextPage = () => {
       <div className='discord-bg'>
         <h1 className="">FF4 Ultima Discord</h1>
           <BothTitles />
-          <h2>⬇</h2>
-          <a href="https://discord.gg/PGMASbSnD9" target='blank'>
-          <button
-            className="nicer-btn-blue px-5 py-3 hover:bg-blue-700">
-              Join us in the FF4 Ultima Discord
-            </button>
-          </a>
-          <h2>⬆</h2>
+          <div className='mobile-layout-title-fix'>
+            <h2>⬇</h2>
+            <a href="https://discord.gg/PGMASbSnD9" target='blank'>
+            <button
+              className="nicer-btn-blue px-5 py-3 hover:bg-blue-700">
+                Join us in the FF4 Ultima Discord
+              </button>
+            </a>
+            <h2>⬆</h2>
+          </div>
       </div>
     </Layout>
   );
--- pages/guides.tsx
+++ pages/guides.tsx
@@ -8,58 +8,66 @@ const Guides: NextPage = () => {
       <div className='guides-bg'>
         <h1 className="">FF4 Ultima Guides</h1>
           <BothTitles />
-        <h3>Guides written by Lily of Team Ultima</h3>
-        <ul>
-        	<li>
-			    <a target="_blank" href="https://docs.google.com/document/d/1O77TDKTG1dWDrHaT40NECik6Cz3rtYmUoazxTQPxnIY/edit?tab=t.0">
-				    <h3>FF4U Commands Guide</h3>
-			    </a>
-			</li>
-			<li>
-				<a target="_blank" href="https://docs.google.com/document/d/1Z4O08DPN0JhQ4E0d2wbQYOZM4n814V5DyhyWqTVPSkM/edit?usp=sharing">
-					<h3>FF4U Overall Guide</h3>
-				</a>
-			</li>
-			<li>
-				<a target="_blank" href="https://docs.google.com/spreadsheets/d/19IU_ccvRXbx7BrihScF6c0JQ-7mFVNQR96KukOGrXkU/edit?gid=589265229#gid=589265229">
-					<h3>FF4U Boss Guide</h3>
-				</a>
-			</li>
-			<li>
-				<a target="_blank" href="https://docs.google.com/spreadsheets/d/1NDR7HuKJXEq6X0EsmXlQ52uE9wctkn9qvZKSTYc9bcI/edit?gid=0#gid=0">
-					<h3>FF4U Drop/Steal Table</h3>
-				</a>
-			</li>
-			<li>
-			    <a target="_blank" href="https://docs.google.com/spreadsheets/d/1gxxapWwliJwzvp8TSYJL2qRT3vvJhZciRpN0bXcEDFU/edit?gid=40660744#gid=40660744">
-				    <h3>FF4U Item Checklist</h3>
-			    </a>
-		    </li>
-        </ul>
-		<h4>Maps charted by Boomerang</h4>
-		<ul>
-			<li>
-				<a target="_blank" href="/overworldmap">
-					<h3>
-						Overworld Map
-					</h3>
-				</a>
-			</li>
-			<li>
-				<a target="_blank" href="/underworldmap">
-					<h3>
-						Underworld Map
-					</h3>
-				</a>
-			</li>
-			<li>
-				<a target="_blank" href="/lunarmap">
-					<h3>
-						Lunar Map
-					</h3>
-				</a>
-			</li>
-		</ul>
+        <div className='dark-grey-panel mobile-layout-title-fix'>
+			<h3>Guides written by Lily of Team Ultima</h3>
+			<ul>
+				<li>
+					<a target="_blank" href="https://docs.google.com/document/d/1O77TDKTG1dWDrHaT40NECik6Cz3rtYmUoazxTQPxnIY/edit?tab=t.0">
+						<h3>FF4U Commands Guide</h3>
+					</a>
+				</li>
+				<li>
+					<a target="_blank" href="https://docs.google.com/document/d/1Z4O08DPN0JhQ4E0d2wbQYOZM4n814V5DyhyWqTVPSkM/edit?usp=sharing">
+						<h3>FF4U Overall Guide</h3>
+					</a>
+				</li>
+				<li>
+					<a target="_blank" href="https://docs.google.com/spreadsheets/d/19IU_ccvRXbx7BrihScF6c0JQ-7mFVNQR96KukOGrXkU/edit?gid=589265229#gid=589265229">
+						<h3>FF4U Boss Guide</h3>
+					</a>
+				</li>
+				<li>
+					<a target="_blank" href="https://docs.google.com/spreadsheets/d/1NDR7HuKJXEq6X0EsmXlQ52uE9wctkn9qvZKSTYc9bcI/edit?gid=0#gid=0">
+						<h3>FF4U Drop/Steal Table</h3>
+					</a>
+				</li>
+				<li>
+					<a target="_blank" href="https://docs.google.com/spreadsheets/d/1gxxapWwliJwzvp8TSYJL2qRT3vvJhZciRpN0bXcEDFU/edit?gid=40660744#gid=40660744">
+						<h3>FF4U Item Checklist</h3>
+					</a>
+				</li>
+				<li>
+					<a target="_blank" href="https://docs.google.com/spreadsheets/d/154hnvdQEgOl5nOxxU2RvFwTlcP8lIRasSZMJVySvpM0/edit?gid=1386834576#gid=1386834576">
+						<h3>FF4U Bestiary</h3>
+					</a>
+				</li>
+
+			</ul>
+			<h4>Maps charted by Boomerang</h4>
+			<ul>
+				<li>
+					<a target="_blank" href="/overworldmap">
+						<h3>
+							Overworld Map
+						</h3>
+					</a>
+				</li>
+				<li>
+					<a target="_blank" href="/underworldmap">
+						<h3>
+							Underworld Map
+						</h3>
+					</a>
+				</li>
+				<li>
+					<a target="_blank" href="/lunarmap">
+						<h3>
+							Lunar Map
+						</h3>
+					</a>
+				</li>
+			</ul>
+		</div>
 		<h4>Bestiary Omissions</h4>
 		<ul>
 			<li>The 4 Fiends fight in the Giant</li>
@@ -70,6 +78,7 @@ const Guides: NextPage = () => {
 			<li>Dark Elf when you show up WITHOUT Whisperweed</li>
 			<li>The Zeromus fought by Golbez & FuSoYa in the cutscene at the end</li>
 		</ul>
+		<h3 className='classic-hr>'>ᕦ(ò_óˇ)ᕤ</h3>
 		<h4>Rom Offsets for Sprite Hacking</h4>
 		<ul>
 			<li>
