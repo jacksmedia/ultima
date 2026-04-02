8fbc8ad62cfb19ab65e78a044a08011711e2389d
jacksmedia <alex@j4cks.com>
2026-04-02 15:33:44 -0700

    🔧smaller footer text

--- components/Attribution.tsx
+++ components/Attribution.tsx
@@ -2,10 +2,10 @@ const Attribution = () => {
     return(
     <>
       <div className='mt-5 w-full block-inline bg-black fixed-bottom-div'>
-          <p className="text-center">Not endorsed by Square Enix, and not affiliated with them in any way. 
+          <p className="text-center small-text">Not endorsed by Square Enix, and not affiliated with them in any way. 
           Site built by <a href="https://jacks.media" rel="noopener noreferrer" target="_blank" className="special">Jacks.Media</a>, 2026.</p>
           
-          <p className="text-center"><a target="_blank" href="https://icons8.com/icon/pfF6HpODcjW0/close">Menu</a> icons by <a target="_blank" href="https://icons8.com">Icons8</a></p>
+          <p className="text-center small-text"><a target="_blank" href="https://icons8.com/icon/pfF6HpODcjW0/close">Menu</a> icons by <a target="_blank" href="https://icons8.com">Icons8</a></p>
       </div>
       <style jsx>{`
         .fixed-bottom-div {
@@ -17,6 +17,9 @@ const Attribution = () => {
           0% {color: #80a;}
           50% {color: #f00;}
           100% {color: #80a;}
+        }
+        .small-text {
+          font-size: small;
         }
         .special {
           color: #80a;
